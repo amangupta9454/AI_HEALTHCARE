93ee9cd7194151d62ba8f26aa130d15e3cb181e5
AMAN GUPTA <ag0567688@gmail.com>
2025-06-01 18:50:44 +0530

    mongo

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log('MONGODB_URI:', process.env.MONGO_URI ? '****' : 'undefined');
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGODB_URI is not defined in .env file');
+    }
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
-    console.log('MONGO_URI:', process.env.MONGO_URI); // Log the MONGO_URI for debugging
-  } catch (err) {
-    console.error(err.message);
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
   }
 };
